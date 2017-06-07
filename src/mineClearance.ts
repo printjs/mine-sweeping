b0234354b07c0434e65a0c3e943ea1fa58de4ffd
wjt <wujiantao@swad.com>
2017-06-07 17:11:28 +0800

    解决typescript中require的问题。以及加入less

--- src/mineClearance.ts
+++ src/mineClearance.ts
@@ -1,10 +1,25 @@
-import {Checkerboard} from './checkBoard'
+import {Checkerboard} from './checkBoard';
+var style:any = require('./less/main');
 
-
-let test = new Checkerboard("ninetyNine");
-console.log(test.checkBoard);
 class mineClearance {
-  constructor(){
-    
+  private mine:Array<Array<number>>;
+  constructor(type:string){
+    this.mine = new Checkerboard(type).checkBoard;
+    this.renderMine();
+  }
+
+  private renderMine (){
+    let root = document.getElementById('mineArea');
+    for(let i = 0,len = this.mine.length;i<len;i++){
+      for(let j = 0,len = this.mine[i].length;j<len;j++){
+        let div = document.createElement('div');
+        div.setAttribute("data-val",this.mine[i][j]+"");
+        div.setAttribute("class","unknow");
+        root.appendChild(div);
+      }
+    }
   }
 }
+
+new mineClearance("ninetyNine");
+// console.log(test.mine);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   // click on the name of the option to get to the detailed documentation
   // click on the items with arrows to show more examples / advanced options
 
-  entry: "./src/mineClearance", // string | object | array
+  entry: {app:"./src/mineClearance"}, // string | object | array
   // Here the application starts executing
   // and webpack starts bundling
 
@@ -16,18 +17,6 @@ module.exports = {
     // must be an absolute path (use the Node.js path module)
 
     filename: "[name].js", // string
-    // the filename template for entry chunks
-
-    publicPath: "/assets/", // string
-    // the url to the output directory resolved relative to the HTML page
-
-    library: "MyLibrary", // string,
-    // the name of the exported library
-
-    libraryTarget: "umd", // universal module definition
-    // the type of the exported library
-
-    /* Advanced output configuration (click to show) */
   },
 
   module: {
@@ -36,14 +25,24 @@ module.exports = {
     rules: [
       // rules for modules (configure loaders, parser options, etc.)
       {
-        test:/\.ts?$/,
-        include:[
+        test: /\.ts?$/,
+        include: [
           path.resolve(__dirname, "src")
         ],
         exclude: [
           path.resolve(__dirname, "node_modules")
         ],
         loader: "awesome-typescript-loader",
+      },
+      {
+        test: /\.less$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "less-loader" // compiles Less to CSS
+        }]
       }
     ],
 
@@ -60,11 +59,10 @@ module.exports = {
     ],
     // directories where to look for modules
 
-    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js",".less"],
     // extensions that are used
 
-    alias: {
-    },
+    alias: {},
   },
 
   devtool: "source-map", // enum
